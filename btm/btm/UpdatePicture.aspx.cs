6468962e78237ee14f4eb3451c882b9ed899c4a9
yuvaloni <yuvon2@gmail.com>
2014-06-16 19:11:13 +0300

    dfg

--- btm/btm/UpdatePicture.aspx.cs
+++ btm/btm/UpdatePicture.aspx.cs
@@ -17,6 +17,8 @@ namespace btm
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!Directory.Exists(Path.Combine(Server.MapPath("~"), "Users")))
+                Directory.CreateDirectory(Path.Combine(Server.MapPath("~"), "Users"));
             if (Session["user"] == null)
                 Response.Redirect("http://btm.apphb.com/login.aspx?ref=UpdatePicture");
         }
