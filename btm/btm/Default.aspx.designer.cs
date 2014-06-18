0d79c293981fc753655a2869a694e1b6b3aa2ac3
yuvaloni <yuvon2@gmail.com>
2014-06-18 19:03:00 +0300

    sdf

--- btm/btm/Default.aspx.designer.cs
+++ btm/btm/Default.aspx.designer.cs
@@ -130,15 +130,6 @@ namespace btm {
         protected global::System.Web.UI.WebControls.TextBox TextBox2;
         
         /// <summary>
-        /// TextBox3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox3;
-        
-        /// <summary>
         /// doLogin control.
         /// </summary>
         /// <remarks>
